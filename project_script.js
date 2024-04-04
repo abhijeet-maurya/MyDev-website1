b1e7dbe4679eb5b2111eb91a16810538dce5ea23
Abhijeet Maurya <abhijeetmaurya0007.com>
2024-04-04 12:44:42 +0530

    project img slider buttons added

--- project_script.js
+++ project_script.js
@@ -1,15 +1,42 @@
 
-gsap.from('.img_slider', {
-    duration:16,
-    x: "-440%",
-    scrubber:3,
-    yoyo:true,
-    repeat:-1,
-    // scrollTrigger:{
-    //     trigger:'#cantainer_img',
-    //     markers:true,
-    //     start:'Top 60%',
-    //     end:'Top 10%',
-    //     pin:true,
-    // }
-});
+// gsap.from('.img_slider', {
+//     duration:16,
+//     x: "-440%",
+//     scrubber:3,
+//     yoyo:true,
+//     repeat:-1,
+//     scrollTrigger:{
+//         trigger:'#cantainer_img',
+//         // markers:true,
+//         start:'Top 60%',
+//         end:'Top 10%',
+//         pin:true,
+//     }
+// });
+
+let a=0;
+let LBtnCount= 0;
+let RBtnCount= 0;
+
+function left(){
+    a=a+110;
+    if(a>(-350)){
+        document.querySelector('.img_slider').style.transform=`translateX(${a}%)`;
+    }
+    else{
+        a=0;
+        document.querySelector('.img_slider').style.transform=`translateX(${a}%)`;
+    }
+}
+
+function right(){
+    a=a-110;
+    if(a>(-350)){
+        document.querySelector('.img_slider').style.transform=`translateX(${a}%)`;
+    }
+    else{
+        a=0;
+        document.querySelector('.img_slider').style.transform=`translateX(${a}%)`;
+    }
+}
+
